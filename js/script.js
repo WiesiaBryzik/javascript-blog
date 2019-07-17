22b0c7f0af7114c3381f5af6f970dd94a0cf83d2
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2019-07-17 10:20:50 +0200

    GenerateTitleLinks

--- js/script.js
+++ js/script.js
@@ -42,9 +42,49 @@
         targetArticle.classList.add('active');
     }
 
-    const links = document.querySelectorAll('.titles a');
+        const optArticleSelector = '.post',
+        optTitleSelector = '.post-title',
+        optTitleListSelector = '.titles';
 
-    for (let link of links) {
-        link.addEventListener('click', titleClickHandler);
+    function generateTitleLinks() {
+
+        /* remove contents of titleList */
+        const titleList = document.querySelector(optTitleListSelector);
+        titleList.innerHTML = '';
+     
+        /* for each article */
+        const articles = document.querySelectorAll(optArticleSelector);
+
+        let html = '';
+
+        for (let article of articles) {
+
+            /* get the article id */
+            const articleId = article.getAttribute('id');
+            console.log(articleId);
+
+            /* find the title element */
+            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+            /* get the title from the title element */
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+            console.log(linkHTML);
+
+            /* create HTML of the link */
+
+            /* insert link into titleList */
+            html = html + linkHTML;
+        }
+
+        titleList.innerHTML = html;
+
+        const links = document.querySelectorAll('.titles a');
+        console.log(links);
+
+        for (let link of links) {
+            link.addEventListener('click', titleClickHandler);
+        }
     }
+
+    generateTitleLinks();
 }
